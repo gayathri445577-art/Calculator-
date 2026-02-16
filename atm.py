0a24917e5f162655a3a4d161d3b39c74e269fb38
gayathri445577-art <gayathri445577@gmail.com>
2026-02-16 18:47:21 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,9 @@
+amount=int(input("enter amount"))
+balance=5000;
+if amount>=balance:
+	print("insufficient funds")
+if amount<=balance:
+		print("sufficient funds")
+		print("amount is withdrawl")
+else:
+			print("valid")
