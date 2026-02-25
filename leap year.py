1a3b3e4d8a18f3dd842a57e5576dcbe05ca246d3
gayathri445577-art <gayathri445577@gmail.com>
2026-02-25 17:50:32 +0530

    Create leap year.py

---
+++ leap year.py
@@ -0,0 +1,7 @@
+#check whether a year ia leap year  or not
+a=input("enter the year")
+g=int(input( "enetr number of days"))
+if g==366:
+	print("the year is leap year",a)
+else:
+	print("it is not a leap year")
