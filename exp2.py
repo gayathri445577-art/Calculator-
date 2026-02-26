f834836754fab36420313813c1a7dfec1f9f45b4
gayathri445577-art <gayathri445577@gmail.com>
2026-02-26 18:17:38 +0530

    Create exp2.py

---
+++ exp2.py
@@ -0,0 +1,7 @@
+#Find the greatest of two numbers.
+a=int(input("enter the number"))
+g=int(input("enter the number"))
+if a>g or g>a:
+	print("it is greater number")
+elif a==g:
+	print(" the two are equal numbers")
