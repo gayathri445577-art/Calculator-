9093276eefde9fb071d90e4c2abbb61fbd8b8b21
gayathri445577-art <gayathri445577@gmail.com>
2026-02-26 18:07:54 +0530

    Create exp1.py

---
+++ exp1.py
@@ -0,0 +1,6 @@
+#Check whether a number is divisible by 5 and 11
+a=int(input("enter a number"))
+if a%5 &a%11==0:
+	print("number is divisible by 5 and 11 is :",a)
+else:
+	print("number doesn't divisible by 5 and 11")
