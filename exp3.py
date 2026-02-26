6190a76d581830a3d0d689310d16ead892358517
gayathri445577-art <gayathri445577@gmail.com>
2026-02-26 18:33:59 +0530

    Create exp3.py

---
+++ exp3.py
@@ -0,0 +1,11 @@
+##Check whether a character is:
+#Alphabet
+#Digit
+#Special character
+a=input("enter ")
+if 'a'<=a<='z' or 'A' <= a <= 'Z':
+	print("it is a alphabet")
+elif '0' <= a<= '9':
+	print("it is digit")
+else:
+	print(" it is special charayer")
