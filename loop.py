bfb7cdce09c0f55993a8219f940a798c232dfae4
gayathri445577-art <gayathri445577@gmail.com>
2026-03-03 19:18:50 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,14 @@
+#loop:
+	the loop is a control structure that is used to  execute  a block of code repeatedly
+	#control structure:
+		loop is a control.structure because of it control the flow orexecution of program.
+#two types:
+	for loop:
+		number of iterations known
+	while loop:
+		number of iterations depend on decision
+	eg- 
+i = 1
+while i < 6:
+  print(i)
+  i += 1
