1b941d26fa787f645dd56d62249170e016919dd3
gayathri445577-art <gayathri445577@gmail.com>
2026-03-10 22:41:50 +0530

    Create loop35.py

---
+++ loop35.py
@@ -0,0 +1,8 @@
+#write a program to find maximum and minimum element in list using while loop
+a = [2,3,4,54,67,32,90,67]
+while len(a) > 0:
+    c = max(a)
+    d = min(a)
+    break
+print("the max is :", c)
+print("the min is :", d)
