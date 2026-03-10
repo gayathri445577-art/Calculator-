835fd3eff24b41ea24c443803a95eafa4c592446
gayathri445577-art <gayathri445577@gmail.com>
2026-03-10 17:42:05 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,10 @@
+#implement stack using list and perform push and pop operation
+stack=[ ]
+stack.append(10)
+stack.append(20)
+stack.append(35)
+print(stack)
+e=stack.pop(-2)
+print("removed element is",e)
+print(stack)
+	
