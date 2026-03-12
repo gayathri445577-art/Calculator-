f386f00803d972b70a1b876ee42d4b795e562b75
gayathri445577-art <gayathri445577@gmail.com>
2026-03-12 21:33:57 +0530

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,8 @@
+#for loop:
+the for loop is a repeated block of code and each item in a sequence 
+for loop=do something again and again for every item in sequence way or ordered way
+ex-list,tuple,set..
+ex:-
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
