049acdee885db3ff962c3903a7f293c0a42e7441
gayathri445577-art <gayathri445577@gmail.com>
2026-03-12 21:48:59 +0530

    Create for6.py

---
+++ for6.py
@@ -0,0 +1,7 @@
+Else in For Loop:-
+The else keyword in a for loop specifies a block of code to be executed when the loop is finished.
+ex:-
+for x in range(20):
+    print(x)
+else:
+    print("finished!")
