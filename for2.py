9d2b7a71c312ac8f5587d445290b246257f7fa34
gayathri445577-art <gayathri445577@gmail.com>
2026-03-12 21:38:13 +0530

    Create for2.py

---
+++ for2.py
@@ -0,0 +1,9 @@
+for loop:-
+break statement:-
+the break statement is used for to stop the loop .if condition is true 
+ex:-
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
