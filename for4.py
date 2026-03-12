f9099b3545a7b4cf2900bdd4b1b070b26ed73826
gayathri445577-art <gayathri445577@gmail.com>
2026-03-12 21:41:26 +0530

    Create for4.py

---
+++ for4.py
@@ -0,0 +1,8 @@
+continue statement:-
+With the continue statement we can stop the current iteration of the loop and continue with the next.
+ex:-
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x) 
