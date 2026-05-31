e365e09f3009a55d7db59d2e51e6a5314a2283c5
gayathri445577-art <gayathri445577@gmail.com>
2026-05-31 20:25:40 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,13 @@
+#functions in python
+a function is a block of code and it is run when it's calling
+# it is used for without repetition code and it is a code resuability
+# a function is created by "def" kwyword
+
+#syntax of function
+def function_name( ):
+#calling fuction :
+function_name
+eg--: 
+def my_function():
+  print("Hello from a function")
+my_function()
